1765c25f7be1b615add469b17c4039a37bf99add
Hendi Santika <hendisantika@gmail.com>
2018-11-28 22:52:19 +0700

    #10 - Modify Main Class

--- src/main/java/com/hendisantika/uploaddownload/SpringBootUploadDownloadFileRestApiExampleApplication.java
+++ src/main/java/com/hendisantika/uploaddownload/SpringBootUploadDownloadFileRestApiExampleApplication.java
@@ -1,9 +1,14 @@
 package com.hendisantika.uploaddownload;
 
+import com.hendisantika.uploaddownload.property.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+        FileStorageProperties.class
+})
 public class SpringBootUploadDownloadFileRestApiExampleApplication {
 
     public static void main(String[] args) {
